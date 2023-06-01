d54c1bb1b6e4c2f7751af6c63278a94c9289fa1f
evgeniy <ad121e@mail.ru>
2023-06-01 16:56:45 +0300

    Refactor code

--- Level/field_generator.cpp
+++ Level/field_generator.cpp
@@ -14,14 +14,14 @@ void dfs(int x, int y, std::vector<std::vector<bool>> &field) {
 std::vector<std::vector<bool>> generate::generateField(int height, int width) {
     std::vector<std::vector<bool>> field(height, std::vector<bool>(width, true));
     srand(time(NULL));
-    std::vector<bool> usedx(height, false);
-    std::vector<bool> usedy(width, false);
-    const int CNT_SQARE = 5; //количество прямоугольников, больше 5 не особо работает, лучше оставить на 5
-    // с размерами можно поиграться для красоты карты
-    const int MIN_LENGTH = 7; //Минимальная ширина прямоугольника
-    const int MIN_HEIGHT = 7; //Минимальная высотка прямоуг
+    std::vector<bool> useDx(height, false);
+    std::vector<bool> useDy(width, false);
+    const int CNT_SQARE = 5;
+
+    const int MIN_LENGTH = 7;
+    const int MIN_HEIGHT = 7;
     for (int cnt = 0; cnt < CNT_SQARE;) {
-        //генерация координат прямоугольника между границами всего поля
+
         int x1 = rand();
         int x2 = rand();
         int y1 = rand();
@@ -36,26 +36,24 @@ std::vector<std::vector<bool>> generate::generateField(int height, int width) {
         x2++;
         y1++;
         y2++;
-        // Проверка на минимальные растояния прямоугольника
         if (abs(x1 - x2) < MIN_HEIGHT || abs(y1 - y2) < MIN_LENGTH) {
             continue;
         }
 
-        //Чтобы не генерировались друг на друге прямоугольники usedx и usedy заполняем и проверяем на совпадение
         bool ok = true;
         for (int i = 0; i < 2; i++) {
-            if (usedx[x1 + i] || usedx[x2 + i] || usedy[y1 + i] || usedy[y2 + i]) {
+            if (useDx[x1 + i] || useDx[x2 + i] || useDy[y1 + i] || useDy[y2 + i]) {
                 ok = false;
             }
         }
         if (!ok) { continue; }
         for (int i = 0; i < 2; i++) {
-            usedx[x1 + i] = true;
-            usedx[x2 + i] = true;
-            usedy[y1 + i] = true;
-            usedy[y2 + i] = true;
+            useDx[x1 + i] = true;
+            useDx[x2 + i] = true;
+            useDy[y1 + i] = true;
+            useDy[y2 + i] = true;
         }
-        //заполнение поля
+
         for (auto x: {x1, x2}) {
             for (int y = std::min(y1, y2); y <= std::max(y1, y2); y++) {
                 field[x][y] = false;
@@ -69,7 +67,6 @@ std::vector<std::vector<bool>> generate::generateField(int height, int width) {
         cnt++;
 
     }
-    //проверка на связность
     auto temp = field;
     int cnt = 0;
     for (int i = 1; i < height - 1; i++) {
@@ -80,7 +77,6 @@ std::vector<std::vector<bool>> generate::generateField(int height, int width) {
             }
         }
     }
-    //если не связный переделать
     if (cnt > 1) {
         return generateField(height, width);
     }
